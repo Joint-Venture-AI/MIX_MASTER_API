302d7c0abc8489c7708f3cbd5703a9c1d3d41d3e
rakib3421 <rakibul15-3430@diu.edu.bd>
2025-07-14 14:44:45 +0600

    check

--- half_cocktail.py
+++ half_cocktail.py
@@ -4,6 +4,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
 from openai import OpenAI
+#fuck it
 
 # Load environment variables
 load_dotenv()
